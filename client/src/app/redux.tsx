eae9298eeddbcb3135989ce4c7caca4103942c55
sundayuchegbu <sundayuchegbu10@gmail.com>
2025-04-30 00:51:09 +0100

    removed any for unknown

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -29,7 +29,7 @@ const createNoopStorage = () => {
     getItem() {
       return Promise.resolve(null);
     },
-    setItem(value: any) {
+    setItem(value: unknown) {
       return Promise.resolve(value);
     },
     removeItem() {
